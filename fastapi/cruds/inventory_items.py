dc96cc8a4d762c1dc22d4e727c8368e793b0b130
t_ohashi <o.tsukasa.24@gmail.com>
2025-05-01 23:27:52 +0900

    在庫登録 ・在庫管理物の新規登録機能　作成

--- fastapi/cruds/inventory_items.py
+++ fastapi/cruds/inventory_items.py
@@ -2,39 +2,18 @@ from typing import List
 from uuid import UUID
 import core.errors as errors
 from database.database import SessionLocal
-from models.account import Account
-from fastapi.models.inventory_items import InventoryItem
+from models.inventory_items import InventoryItem
 from sqlalchemy.orm import Session, joinedload
 
-"""def get_multi(db: Session, skip: int = 0, limit: int = 10) -> List[User]:
-    try:
-        # 全件の件数取得
-        total_count = db.query(User).join(Account).filter(Account.is_deleted != True).count()
-        # accountのis_deletedがFalseのものだけ取得
-        items = db.query(User).join(Account).filter(Account.is_deleted != True).offset(skip).limit(limit).all()
-    except Exception as e:
-        print(e)
-        raise errors.InternalServerError('ユーザー一覧取得に失敗しました。', e)
+def create_items(db: Session, item_name: str, item_stock: int, order_threshold: int):
 
-    if len(items) == 0:
-        raise errors.NotFoundError('ユーザーが見つかりませんでした。')
+    new_item = InventoryItem(
+            item_name=item_name,
+            item_stock=item_stock,
+            order_threshold=order_threshold
+        )
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
 
-    response = {
-        'offset': skip,
-        'count': len(items),
-        'totalCount': total_count,
-        'data': items
-    }
-
-    return response
-
-
-def get_user_by_id(db: Session, user_id: int):
-    try:
-        user = db.query(User).filter(User.id == user_id).first()
-        return user
-
-    except Exception as e:
-        print(e)
-        raise errors.InternalServerError('ユーザー情報取得に失敗しました。', e)
-"""
+    return new_item
--- fastapi/routers/dashboard.py
+++ fastapi/routers/dashboard.py
@@ -1,6 +1,9 @@
 import core.errors as errors
-import schemas.account as schemas_account
+import schemas.inventory_items as schemas_inventory_itemBase
+import cruds.inventory_items as crud_inventory_items
 from fastapi import APIRouter, Depends
+from database.database import get_db
+from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter(
@@ -12,14 +15,23 @@ router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="signin")
 
 
-@router.get(
+@router.post(
     "/",
-    summary="ログイン認証",
-    response_description="emailとパスワードを使用したログイン認証",
-    response_model = schemas_account.CreateAccount,
+    summary="在庫管理物登録",
+    response_description="在庫で管理している購入品等の新規登録",
+    response_model = schemas_inventory_itemBase.InventoryItemBase,
     response_model_exclude_none=True,
     responses=errors.error_response([errors.NotFound, errors.InvalidParameter, errors.InternalServerError])
 )
 
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+async def create_items(
+    item_name: str,
+    item_stock: int,
+    order_threshold: int,
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+    ):
+
+    new_item = crud_inventory_items.create_items(db, item_name, item_stock, order_threshold)
+
+    return new_item
